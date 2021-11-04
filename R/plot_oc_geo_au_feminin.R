45a2193cef54557753ce4a4c35a348edf1e95248
Pascal Burkhard <pascal.burkhard@gmail.com>
2021-11-04 14:39:07 +0100

    Add swiss abortion graph

--- R/plot_oc_geo_au_feminin.R
+++ R/plot_oc_geo_au_feminin.R
@@ -32,3 +32,41 @@ oc_geo_au_feminin_graph_scolarisation_filles_afghanistan <- function(theme = ggp
     ) +
     theme
 }
+
+#' OC Géographie au féminin: taux d'avortement en Suisse
+#'
+#' @param theme A ggplot2 theme
+#'
+#' @return A ggplot2 graph
+#' @export
+oc_geo_au_femini_graph_suisse_avortements <- function(theme = ggplot2::theme_minimal()) {
+  geodata::oc_geo_au_feminin_2021_ofs_avortements |>
+    ggplot2::ggplot(ggplot2::aes(year, rate, color = type)) +
+    ggplot2::geom_line(size = 2.5) +
+    paletteer::scale_color_paletteer_d(
+      "nord::lumina",
+      labels = c("de 15 à 49 ans", "de 15 à 19 ans")
+    ) +
+    ggplot2::scale_y_continuous(limits = c(0, NA)) +
+    ggplot2::scale_x_continuous(breaks = seq(2004, 2020, 2)) +
+    theme +
+    ggplot2::theme(legend.position = "bottom") +
+    ggplot2::labs(
+      title = "Interruptions de grossesse",
+      subtitle = "Taux pour 1000 femmes",
+      x = "", y = "", color = "",
+      caption = "OFS (2021)"
+    )
+}
+
+oc_geo_au_feminin_graph_rapport_hommes_femmes_2020 <- function(theme = ggplot2::theme_minimal()) {
+  geodata::oc_geo_au_feminin_wpp2019_sex_ratio |>
+    dplyr::filter(
+      un_code %in% c(900, 903, 935, 908, 905, 909, 904),
+      year == 2020
+    ) |>
+    dplyr::mutate(age = forcats::fct_inorder(age)) |>
+    dplyr::select(age, ratio, name) |>
+    ggplot2::ggplot(ggplot2::aes(x = age, y = ratio, color = name, group = name)) +
+    ggplot2::geom_line()
+}
