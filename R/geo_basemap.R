2796237c260ab495630107945cf74d80da336fab
Pascal Burkhard <pascal.burkhard@gmail.com>
2023-09-10 22:13:54 +0200

    Update gph_basemap

--- R/geo_basemap.R
+++ R/geo_basemap.R
@@ -9,8 +9,13 @@ gph_basemap <- function(country) {
     scale = 10, country = country,
     returnclass = "sf")
 
-  tmap::tmap_mode("view")
-  tmap::tm_basemap(leaflet::providers$Stadia.OSMBright) +
-    tmap::tm_shape(border) +
-    tmap::tm_borders(col = "red", lwd = 2, group.control = "none")
+  leaflet::leaflet() |>
+    leaflet::addProviderTiles(leaflet::providers$Stadia.OSMBright) |>
+    leaflet::addPolygons(data = border, color = "red", weight = 2)
+
+  # The tmap version doesn't work in Quarto for now…
+  # tmap::tmap_mode("view")
+  # tmap::tm_basemap(leaflet::providers$Stadia.OSMBright) +
+  #   tmap::tm_shape(border) +
+  #   tmap::tm_borders(col = "red", lwd = 2, group.control = "none")
 }
