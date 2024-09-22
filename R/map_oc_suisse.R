1fa5943770ce262108e6609a12e53417906d73f5
Pascal Burkhard <pascal.burkhard@gmail.com>
2024-09-22 22:22:08 +0200

    feat: add Swiss religion map

--- R/map_oc_suisse.R
+++ R/map_oc_suisse.R
@@ -110,3 +110,161 @@ oc_suisse_carte_1970_suffrage_feminin_valais <- function(theme = ggplot2::theme_
     ) +
     theme
 }
+
+#' Religion par commune
+#'
+#' @returns A highcharts map
+#' @export
+#'
+#' @examples
+#' oc_suisse_carte_hc_2020_religion()
+oc_suisse_carte_hc_2020_religion <- function() {
+  geodata::oc_suisse_2000_religion |>
+    dplyr::select(-Commune) |>
+    dplyr::filter(Religion != "Total") |>
+    dplyr::group_by(ID) |>
+    dplyr::arrange(dplyr::desc(Pourcentage)) |>
+    dplyr::summarise(
+      Religion = dplyr::first(Religion),
+      Pourcentage = dplyr::first(Pourcentage)
+    ) |>
+    dplyr::mutate(value = round(Pourcentage, 2)) -> data
+
+  themakart::thema_map("inst", "polg", 2000, "gf") |>
+    dplyr::mutate(mun_id = as.numeric(id)) -> swiss
+
+  swiss |>
+    dplyr::left_join(data, dplyr::join_by(mun_id == ID)) |>
+    dplyr::filter(Religion == "Eglise catholique romaine") |>
+    geojsonio::geojson_json() -> map_catho
+
+  swiss |>
+    dplyr::left_join(data, dplyr::join_by(mun_id == ID)) |>
+    dplyr::filter(Religion == "Eglise \u00e9evang\u00e9elique r\u00e9eform\u00e9ee") |>
+    geojsonio::geojson_json() -> map_protestant
+
+  swiss |>
+    dplyr::left_join(data, dplyr::join_by(mun_id == ID)) |>
+    dplyr::filter(Religion == "Autres Eglises et communaut\u00e9es protestantes") |>
+    geojsonio::geojson_json() -> map_autre
+
+  swiss |>
+    dplyr::left_join(data, dplyr::join_by(mun_id == ID)) |>
+    dplyr::filter(Religion == "Aucune appartenance") |>
+    geojsonio::geojson_json() -> map_athee
+
+  palette_catho <- function(...) {
+    paletteer::paletteer_c("ggthemes::Green", ...)
+  }
+
+  palette_protestant <- function(...) {
+    paletteer::paletteer_c("ggthemes::Orange", ...)
+  }
+
+  palette_autre <- function(...) {
+    paletteer::paletteer_c("ggthemes::Purple", ...)
+  }
+
+  palette_athee <- function(...) {
+    paletteer::paletteer_c("ggthemes::Gray", ...)
+  }
+
+  highcharter::highchart(type = "map") |>
+    highcharter::hc_add_series(
+      name = "Background",
+      mapData = swiss |> geojsonio::geojson_json(),
+      showInLegend = FALSE,
+      data = c()
+    ) |>
+    highcharter::hc_add_series(
+      mapData = map_catho,
+      data = data |> dplyr::filter(Religion == "Eglise catholique romaine"),
+      joinBy = c("mun_id", "ID"),
+      name = "Catholique",
+      color = "#406975",
+      value = "value"
+    ) |>
+    highcharter::hc_add_series(
+      mapData = map_protestant,
+      data = data |> dplyr::filter(Religion == "Eglise \u00e9evang\u00e9elique r\u00e9eform\u00e9ee"),
+      joinBy = c("mun_id", "ID"),
+      name = "Protestant \u00e9evang\u00e9elique",
+      color = "#729e93",
+      value = "value",
+      colorAxis = 1
+    ) |>
+    highcharter::hc_add_series(
+      mapData = map_autre,
+      data = data |> dplyr::filter(Religion == "Autres Eglises et communaut\u00e9es protestantes"),
+      joinBy = c("mun_id", "ID"),
+      name = "Autres protestants",
+      color = "#475286",
+      value = "value",
+      colorAxis = 2
+    ) |>
+    highcharter::hc_add_series(
+      mapData = map_athee,
+      data = data |> dplyr::filter(Religion == "Aucune appartenance"),
+      joinBy = c("mun_id", "ID"),
+      name = "Ath\u00e9e",
+      color = "#a59da2",
+      value = "value",
+      colorAxis = 4
+    ) |>
+    highcharter::hc_add_series(
+      name = "Lakes",
+      mapData = geodata::gdt_opendata_swiss_geodata("lake") |>
+        sf::st_transform(crs = sf::st_crs(swiss)) |>
+        geojsonio::geojson_json(),
+      showInLegend = FALSE,
+      color = "#8ec6e9",
+      nullColor = "#8ec6e9",
+      data = c()
+    ) |>
+    highcharter::hc_colorAxis(
+      list(
+        dataClasses = geotools::gtl_hc_color_axis(
+          santoku::chop(
+            data |>
+              dplyr::filter(Religion == "Eglise catholique romaine") |>
+              dplyr::pull(value),
+            c(25, 50, 75, 100)
+          ), palette_catho
+        ),
+        showInLegend = FALSE
+      ),
+      list(
+        dataClasses = geotools::gtl_hc_color_axis(
+          santoku::chop(
+            data |>
+              dplyr::filter(Religion == "Eglise \u00e9evang\u00e9elique r\u00e9eform\u00e9ee") |>
+              dplyr::pull(value),
+            c(25, 50, 75, 100)
+          ), palette_protestant
+        ),
+        showInLegend = FALSE
+      ),
+      list(
+        dataClasses = geotools::gtl_hc_color_axis(
+          santoku::chop(
+            data |>
+              dplyr::filter(Religion == "Autres Eglises et communaut\u00e9es protestantes") |>
+              dplyr::pull(value),
+            c(25, 50, 75, 100)
+          ), palette_autre
+        ),
+        showInLegend = FALSE
+      ),
+      list(
+        dataClasses = geotools::gtl_hc_color_axis(
+          santoku::chop(
+            data |>
+              dplyr::filter(Religion == "Aucune appartenance") |>
+              dplyr::pull(value),
+            c(25, 50, 75, 100)
+          ), palette_athee
+        ),
+        showInLegend = FALSE
+      )
+    )
+}
