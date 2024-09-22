c1ce7d56d21afecc3097ab14af5003dc701ab2f9
Pascal Burkhard <pascal.burkhard@gmail.com>
2024-09-22 15:47:58 +0200

    feat: add Russia religion map

--- R/map_oc_russie.R
+++ R/map_oc_russie.R
@@ -256,7 +256,7 @@ oc_russie_carte_evolution_population <- function(theme = ggplot2::theme_minimal(
     ) +
     ggplot2::labs(
       title = "\u00c9volution de la population russe",
-      subtitle = "de 1990 \u00e0 2020, par r\u009gions",
+      subtitle = "de 1990 \u00e0 2020, par r%u009gions",
       fill = "",
       caption = "Source: Rosstat (2020)"
     )
@@ -325,5 +325,190 @@ oc_russie_carte_divorces <- function(theme = ggplot2::theme_minimal()) {
       subtitle = "par r\u00e9gions en 2019 pour 1000 habitants",
       fill = "",
       caption = "Source: Rosstat (2020)"
+    )
+}
+
+#' Carte de la religion majoritaire en 2012
+#'
+#' Religion avec le plus de pourcentage en 2012
+#' par région.
+#'
+#' @returns A higcharts map
+#' @export
+#'
+#' @examples
+#' oc_russie_carte_hc_religion()
+oc_russie_carte_hc_religion <- function() {
+  geodata::oc_russie_2012_religion |>
+    dplyr::select(-region) |>
+    dplyr::group_by(adm1_code) |>
+    dplyr::arrange(-percent) |>
+    dplyr::summarise(
+      religion = dplyr::first(religion),
+      percent = dplyr::first(percent)
+    ) |>
+    dplyr::mutate(value = round(percent, 2)) -> data
+
+  rnaturalearth::ne_states(country = "Russia", returnclass = "sf") -> russia
+
+  russia |>
+    dplyr::left_join(data, by = "adm1_code") |>
+    dplyr::filter(religion == "Orthodoxe") |>
+    geojsonio::geojson_json() -> map_orthodoxe
+
+  russia |>
+    dplyr::left_join(data, by = "adm1_code") |>
+    dplyr::filter(religion == "Islam") |>
+    geojsonio::geojson_json() -> map_islam
+
+  russia |>
+    dplyr::left_join(data, by = "adm1_code") |>
+    dplyr::filter(religion == "Deiste") |>
+    geojsonio::geojson_json() -> map_deiste
+
+  russia |>
+    dplyr::left_join(data, by = "adm1_code") |>
+    dplyr::filter(religion == "Bouddhisme") |>
+    geojsonio::geojson_json() -> map_bouddhisme
+
+  russia |>
+    dplyr::left_join(data, by = "adm1_code") |>
+    dplyr::filter(religion == "Athee") |>
+    geojsonio::geojson_json() -> map_athee
+
+  palette_orthodoxe <- function(...) {
+    paletteer::paletteer_c("ggthemes::Blue", ...)
+  }
+
+  palette_islam <- function(...) {
+    paletteer::paletteer_c("ggthemes::Green", ...)
+  }
+
+  palette_deiste <- function(...) {
+    paletteer::paletteer_c("ggthemes::Purple", ...)
+  }
+
+  palette_bouddhisme <- function(...) {
+    paletteer::paletteer_c("ggthemes::Orange", ...)
+  }
+
+  palette_athee <- function(...) {
+    paletteer::paletteer_c("ggthemes::Gray", ...)
+  }
+
+  highcharter::highchart(type = "map") |>
+    highcharter::hc_add_series(
+      name = "Background",
+      mapData = russia |> geojsonio::geojson_json(),
+      showInLegend = FALSE,
+      data = c()
+    ) |>
+    highcharter::hc_add_series(
+      mapData = map_orthodoxe,
+      data = data |> dplyr::filter(religion == "Orthodoxe"),
+      joinBy = "adm1_code",
+      name = "Orthodoxe",
+      color = "#699cb6",
+      value = "value"
+    ) |>
+    highcharter::hc_add_series(
+      mapData = map_islam,
+      data = data |> dplyr::filter(religion == "Islam"),
+      joinBy = "adm1_code",
+      name = "Islam",
+      color = "#729e93",
+      value = "value",
+      colorAxis = 1
+    ) |>
+    highcharter::hc_add_series(
+      mapData = map_deiste,
+      data = data |> dplyr::filter(religion == "Deiste"),
+      joinBy = "adm1_code",
+      name = "D\u00e9iste",
+      color = "#475286",
+      value = "value",
+      colorAxis = 2
+    ) |>
+    highcharter::hc_add_series(
+      mapData = map_bouddhisme,
+      data = data |> dplyr::filter(religion == "Bouddhisme"),
+      joinBy = "adm1_code",
+      name = "Bouddhisme",
+      color = "#fae3a0",
+      value = "value",
+      colorAxis = 3
+    ) |>
+    highcharter::hc_add_series(
+      mapData = map_athee,
+      data = data |> dplyr::filter(religion == "Athee"),
+      joinBy = "adm1_code",
+      name = "Ath\u00e9e",
+      color = "#a59da2",
+      value = "value",
+      colorAxis = 4
+    ) |>
+    highcharter::hc_mapView(
+      maxZoom = 30,
+      projection = list(
+        name = "Orthographic",
+        rotation = c(-90, -80)
+      )
+    ) |>
+    highcharter::hc_colorAxis(
+      list(
+        dataClasses = geotools::gtl_hc_color_axis(
+          santoku::chop(
+            data |>
+              dplyr::filter(religion == "Orthodoxe") |>
+              dplyr::pull(value),
+            c(25, 35, 45, 55, 65, 75, 85)
+          ), palette_orthodoxe
+        ),
+        showInLegend = FALSE
+      ),
+      list(
+        dataClasses = geotools::gtl_hc_color_axis(
+          santoku::chop(
+            data |>
+              dplyr::filter(religion == "Islam") |>
+              dplyr::pull(value),
+            c(25, 35, 45, 55, 65, 75, 85)
+          ), palette_islam
+        ),
+        showInLegend = FALSE
+      ),
+      list(
+        dataClasses = geotools::gtl_hc_color_axis(
+          santoku::chop(
+            data |>
+              dplyr::filter(religion == "Deiste") |>
+              dplyr::pull(value),
+            c(25, 35, 45, 55, 65, 75, 85)
+          ), palette_deiste
+        ),
+        showInLegend = FALSE
+      ),
+      list(
+        dataClasses = geotools::gtl_hc_color_axis(
+          santoku::chop(
+            data |>
+              dplyr::filter(religion == "Bouddhisme") |>
+              dplyr::pull(value),
+            c(25, 35, 45, 55, 65, 75, 85)
+          ), palette_bouddhisme
+        ),
+        showInLegend = FALSE
+      ),
+      list(
+        dataClasses = geotools::gtl_hc_color_axis(
+          santoku::chop(
+            data |>
+              dplyr::filter(religion == "Athee") |>
+              dplyr::pull(value),
+            c(25, 35, 45, 55, 65, 75, 85)
+          ), palette_athee
+        ),
+        showInLegend = FALSE
+      )
     )
 }
